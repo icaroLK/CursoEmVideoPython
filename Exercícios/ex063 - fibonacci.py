ecde7bfb69da68819dc4b69bf4611d190991561e
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-05-18 11:29:17 -0300

    Update ex063 - fibonacci.py

--- Exercícios/ex063 - fibonacci.py
+++ Exercícios/ex063 - fibonacci.py
@@ -1,8 +1,4 @@
-print('\033[1;97m_\033[m'*19)
-print('     \033[97mFIBONACCI\033[m')
-print('\033[1;97m_\033[m'*19)
 n = int(input('Insira a quantidade de termos: '))
-
 
 
 cont = 1
@@ -10,12 +6,12 @@ qtde = n
 t1 = 0
 t2 = 1
 
-print('{} -> {} -> '.format(t1, t2), end='')
+print('{}, {}, '.format(t1, t2), end='')
 
 while cont <= qtde-2:
     t3 = t1 + t2
     print('{}'.format(t3), end='')
-    print(' -> ' if cont < qtde-2 else '', end='')
+    print(', ' if cont < qtde-2 else '', end='')
     cont += 1
 
     t1 = t2
